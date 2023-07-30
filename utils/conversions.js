68e0c61c3c92f7503808ec8de2267d62b2813c99
Kieron Garvey <96358157@mail.wit.ie>
2023-07-30 23:47:55 +0100

    Updating Icons

--- utils/conversions.js
+++ utils/conversions.js
@@ -1,12 +1,70 @@
+/*
 const weatherCodeMap = new Map();
-weatherCodeMap.set(100, { description: "Clear", icon: "clear-day" });
-weatherCodeMap.set(200, { description: "Partial Clouds", icon: "cloud-up" });
-weatherCodeMap.set(300, { description: "Cloudy", icon: "cloudy" });
-weatherCodeMap.set(400, { description: "Light Showers", icon: "drizzle" });
-weatherCodeMap.set(500, { description: "Heavy Showers", icon: "extreme-rain" });
-weatherCodeMap.set(600, { description: "Rain", icon: "rain" });
-weatherCodeMap.set(700, { description: "Snow", icon: "snow" });
-weatherCodeMap.set(800, { description: "Thunder", icon: "thunderstorms" });
+weatherCodeMap.set(100, { description: "Clear"});
+weatherCodeMap.set(200, { description: "Partial Clouds"});
+weatherCodeMap.set(300, { description: "Cloudy"});
+weatherCodeMap.set(400, { description: "Light Showers"});
+weatherCodeMap.set(500, { description: "Heavy Showers"});
+weatherCodeMap.set(600, { description: "Rain"});
+weatherCodeMap.set(700, { description: "Snow"});
+weatherCodeMap.set(800, { description: "Thunder"});
+*/
+const weatherCodeMap = new Map();
+weatherCodeMap.set(200, { description: "Thunderstorm Light Rain", icon: 200 });
+weatherCodeMap.set(201, { description: "Thunderstorm Rain", icon: 201 });
+weatherCodeMap.set(202, { description: "Thunderstorm Heavy Rain", icon: 202});
+weatherCodeMap.set(210, { description: "Light Thunderstorm", icon: 210 });
+weatherCodeMap.set(211, { description: "Thunderstorm", icon: 200 });
+weatherCodeMap.set(212, { description: "Heavy Thunderstorm", icon: 200 });
+weatherCodeMap.set(221, { description: "Ragged Thunderstorm", icon: 221 });
+weatherCodeMap.set(230, { description: "Thunderstorm Light Drizzle", icon: 230 });
+weatherCodeMap.set(231, { description: "Thunderstorm Drizzle", icon: 231 });
+weatherCodeMap.set(232, { description: "Thunderstorm Heavy Drizzle", icon: 232});
+weatherCodeMap.set(300, { description: "Light Drizzle", icon: 300 });
+weatherCodeMap.set(301, { description: "Drizzle", icon: 301 });
+weatherCodeMap.set(302, { description: "Heavy Drizzle", icon: 302 });
+weatherCodeMap.set(310, { description: "Light Drizzle Rain", icon: 302 });
+weatherCodeMap.set(311, { description: "Drizzle Rain", icon: 313 });
+weatherCodeMap.set(312, { description: "Heavy Drizzle Rain", icon: 313 });
+weatherCodeMap.set(313, { description: "Showers Rain/Drizzle", icon: 313 });
+weatherCodeMap.set(314, { description: "Heavy Shower Rain/Drizzle", icon: 314 });
+weatherCodeMap.set(321, { description: "Shower Drizzle", icon: 314 });
+weatherCodeMap.set(500, { description: "Light Rain", icon: 500 });
+weatherCodeMap.set(501, { description: "Moderate Rain", icon: 501 });
+weatherCodeMap.set(502, { description: "Heavy Rain", icon: 502 });
+weatherCodeMap.set(503, { description: "Very Heavy Rain", icon: 502 });
+weatherCodeMap.set(504, { description: "Extreme Rain", icon: 502 });
+weatherCodeMap.set(511, { description: "Freezing Rain", icon: 511 });
+weatherCodeMap.set(520, { description: "Light Shower Rain", icon: 520 });
+weatherCodeMap.set(521, { description: "Shower Rain", icon: 521 });
+weatherCodeMap.set(522, { description: "Heavy Shower Rain", icon: 522 });
+weatherCodeMap.set(531, { description: "Ragged Shower Rain", icon: 522 });
+weatherCodeMap.set(600, { description: "Light Snow", icon: 600 });
+weatherCodeMap.set(601, { description: "Snow", icon: 600 });
+weatherCodeMap.set(602, { description: "Heavy Snow", icon: 602 });
+weatherCodeMap.set(611, { description: "Sleet", icon: 611 });
+weatherCodeMap.set(612, { description: "Light Sleet Showers", icon: 612 });
+weatherCodeMap.set(613, { description: "Sleet Showers", icon: 613 });
+weatherCodeMap.set(615, { description: "Light Rain and Snow", icon: 613 });
+weatherCodeMap.set(616, { description: "Rain and Snow", icon: 613 });
+weatherCodeMap.set(620, { description: "Light Snow Shower", icon: 620 });
+weatherCodeMap.set(621, { description: "Snow Shower", icon: 621 });
+weatherCodeMap.set(622, { description: "Heavy Snow Shower", icon: 622 });
+weatherCodeMap.set(623, { description: "Flurries", icon: 623 });
+weatherCodeMap.set(701, { description: "Mist", icon: 700 });
+weatherCodeMap.set(711, { description: "Smoke", icon: 711 });
+weatherCodeMap.set(721, { description: "Haze", icon: 721 });
+weatherCodeMap.set(731, { description: "Sand/Dust Whirls", icon: 731 });
+weatherCodeMap.set(741, { description: "Fog", icon: 741 });
+weatherCodeMap.set(751, { description: "Sand", icon: 751 });
+weatherCodeMap.set(762, { description: "Volcanic Ash", icon: 731 });
+weatherCodeMap.set(771, { description: "Squalls", icon: 999 });
+weatherCodeMap.set(781, { description: "Tornado", icon: 781 });
+weatherCodeMap.set(800, { description: "Clear", icon: 800 });
+weatherCodeMap.set(801, { description: "Partial Clouds", icon: 801 });
+weatherCodeMap.set(802, { description: "Scattered Clouds", icon: 802 });
+weatherCodeMap.set(803, { description: "Broken Clouds", icon: 803 });
+weatherCodeMap.set(804, { description: "Overcast Clouds", icon: 804 });
 
 const beaufortLabel = new Map();
 beaufortLabel.set(0, { description: "Calm" });
@@ -174,6 +232,10 @@ export const conversions = {
 
   codeLabel(code) {
     return weatherCodeMap.get(code).description;
+  },
+
+  codeIcon(code) {
+    return weatherCodeMap.get(code).icon;
   },
 
   /**
